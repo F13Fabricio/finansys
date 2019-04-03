f3e71d7d4dd6855882e80878e94c986fc15335e1
Fabricio Farias <fabriciofarias@id.uff.br>
2019-04-02 23:09:58 -0300

    improving category component template

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// primeng imports
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
+
+
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
@@ -9,6 +14,8 @@ import { CategoryFormComponent } from './category-form/category-form.component';
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
+    BreadcrumbModule,
+    ButtonModule,
     CategoriesRoutingModule
   ]
 })
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.sass']
+  styleUrls: ['./category-list.component.sass'],
+  preserveWhitespaces: true
 })
 export class CategoryListComponent implements OnInit {
+
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+      { label: 'Home', routerLink: '/' },
+      { label:'Categories' },
+    ];
   }
 
 }
